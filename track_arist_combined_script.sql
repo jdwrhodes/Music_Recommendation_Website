bc701d1ec940ff8d07bacff47af50bd705c83186
Cailyn <cailynmiller@Cailyns-Air>
2021-06-07 18:15:33 -0500

    updated sql query

--- track_arist_combined_script.sql
+++ track_arist_combined_script.sql
@@ -1,6 +1,6 @@
 CREATE TABLE track_artist_combined AS
 	(SELECT 
 		 t.name AS track_name, t.release_date, t.danceability, t.energy, t.acousticness, t.instrumentalness, t.liveness, t.valence, t.tempo,
-		 a.name AS artist_name, a.genres
+		 a.name AS artist_name, a.genres, a.popularity
 	FROM track_data t, artist a
 	WHERE t.artist_ids = a.artist_id);
