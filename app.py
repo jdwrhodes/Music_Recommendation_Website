afd7ba38cb1801fe92f376a26adcb7aa821b0ff1
Joshua Rhodes <jdw.rhodes@gmail.com>
2021-06-11 19:38:16 -0500

    test add files to heroku

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
+#%%
 from flask import Flask, render_template
+
+#%%
+# Import myMachineLearningModel
+#%%
 app = Flask(__name__)
-
-@app.route('/')
+#%%
+@app.route("/")
 def home():
-   return render_template('index.html')
-if __name__ == '__main__':
-   app.run()
+    return render_template('index.html')
 
+#%%
+@app.route("/how_to")
+def how_to():
+    return render_template('how_to.html')
+
+#%%
+# Route to a webpage with a form
+
+#%%
+# Post route from the form
+    # Call the machine learning model .predict method on the data that was passed from the form
+
+    # Jsonify and python dictionaries to simiply dictionaries being passed (Jsonify)
+        # Remember, Flask is going to look for files inside different folder names that are referenced
+        # return Json and python dictionaries
+        # Ajax calling on the webform will update a div id with the results
