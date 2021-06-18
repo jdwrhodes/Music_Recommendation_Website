d04721e7cecacb7d4083796e3e04a5ba8b8f07ba
Joshua Rhodes <jdw.rhodes@gmail.com>
2021-06-17 20:08:20 -0500

    Added db connection and return result

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 #%%
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import pandas as pd
+from sqlalchemy import create_engine
+from config import db_password
+
+# Connection String
+db_string = f'postgres://nbumduguqxvlys:{db_password}@ec2-54-243-92-68.compute-1.amazonaws.com:5432/ddha93h7jehqkr'
+
+# Create Engine
+engine = create_engine(db_string)
 
 #%%
 # Import myMachineLearningModel
@@ -9,7 +18,22 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+#%%
+@app.route("/", methods=['POST'])
+def home_post():
+    text = request.form['fname']
+    
+    try:
+        db_query = pd.read_sql_query(f"SELECT * FROM magnitude_data_v2 WHERE name = '{text}'", con=engine)
+        
+        db_result = pd.read_sql_query(f"select name, artists, row_num from magnitude_data_v2 where row_num between {db_query['row_num'][0] - 3} and {db_query['row_num'][0] + 3}", con=engine)
 
+        result = f"I think you might like '{db_result['name'][0]}' by '{db_result['artists'][0]}'"
+
+    except:
+        result = "Sorry, we couldn't find that one. Please try again!"
+
+    return render_template('index.html', data = result)
 #%%
 @app.route("/how_to")
 def how_to():
---
+++ config.py
@@ -0,0 +1,2 @@
+# db password
+db_password = 'ec8c43bd6a7604a07ffd201db18ec2464819c03af58a098bba1832445f1234bc'
